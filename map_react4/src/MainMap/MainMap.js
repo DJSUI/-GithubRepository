a135c3bc7c1d5c5c1b2877b461ef1d71983d93d2
SUIDJ <47swagninja@gmail.com>
2024-04-25 14:50:49 +0900

    update

--- map_react4/src/MainMap/MainMap.js
+++ map_react4/src/MainMap/MainMap.js
@@ -32,7 +32,7 @@ import RouteSearch from "./RouteSearch/RouteSearch";
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
-// TODO： 打印点功能加入到mockjs当中，使得地图能显示 点和线 
+
 
 
 const MainMap = () => {
@@ -94,33 +94,53 @@ const MainMap = () => {
     // 图层2----矢量存储在数据库中图层集【循环创建】     
     axios.post('api/FetchRoutes/', { name: NAME })
       .then(response => {
-        let routes = []
-        routes = JSON.parse(JSON.stringify(response.data));
+        let routes = JSON.parse(JSON.stringify(response.data));
+        console.log("routes",routes);
         if (routes !== null) {
-           routes.forEach((route,index) => {
-          const lineCoords = route.map(coord => fromLonLat(coord));
-          const drawSource = new VectorSource();
-          const drawLayer = new VectorLayer({
-            source: drawSource,
-            title: `route${index + 1}`, // 设置图层的标题，可以根据需要进行修改
-            style: new Style({
-              stroke: new Stroke({
-                color: 'blue', // 设置线的颜色
-                width: 4,      // 设置线的宽度
-              }),
-            }),
-          });
-        const lineFeature = new Feature({
-            geometry: new LineString(lineCoords),
-         });
-         drawSource.addFeature(lineFeature);
-             const modify = new Modify({
-              source: drawSource,
-          });
-        map.addInteraction(modify);
-        map.addLayer(drawLayer);
+          routes.forEach((route, index) => {
+            const save_Source = new VectorSource();
+              // 存储点创建
+            route.marker.forEach(coord => {
+              const Fcoord = fromLonLat(coord) 
+              const markerFeature = new Feature({
+                geometry: new Point(Fcoord),
+              });
+              
+              save_Source.addFeature(markerFeature);
+               });
+              // 存储线创建
+            const lineCoords = route.line.map(coord => fromLonLat(coord));
+            const lineFeature = new Feature({
+                geometry: new LineString(lineCoords),
+            });
+            user_edit_vectorSource.addFeature(lineFeature);
+              // 存储层创建 
+            const save_Layer = new VectorLayer({
+              title: `route${index + 1}`,
+              source: save_Source,
+              style: [
+                // 点的样式
+                new Style({
+                  image: new Circle({
+                    radius: 6,
+                    fill: new Fill({ color: 'red' }),
+                    stroke: new Stroke({ color: 'white', width: 2 }),
+                  }),
+                }),
+                // 线的样式
+                new Style({
+                  stroke: new Stroke({
+                    color: 'blue',
+                    width: 4,      
+                  }),
+                }),
+              ],
+            });
+              map.addLayer(save_Layer);    
+            
+          })
           
-        });
+        
         }
 
       })
@@ -128,76 +148,64 @@ const MainMap = () => {
         console.error('获取信息+创建图层失败', error);
       });
 
-    
-    
-    
-    // TODO； 优化将线和点放在一个vector
-    // 图层3--- 用户自定义路径图层 
-      // 增加一个矢量层 用于显示marker 
-      const markerLayer = new VectorLayer({
-        title: "user_edit_marker",
-        source: new VectorSource(),
-        style: new Style({
-          image: new Circle({
-            radius: 6,
-            fill: new Fill({ color: 'red' }),
-            stroke: new Stroke({ color: 'white', width: 2 }),
-          }),
-        }),
+// 图层3--- 用户自定义路径图层集
+  const user_edit_vectorSource = new VectorSource();
+// 创建点的 Feature 并添加到 VectorSource
+  addressList.forEach(coord => {
+      const Fcoord = fromLonLat(coord) 
+      const markerFeature = new Feature({
+        geometry: new Point(Fcoord),
       });
-      // 创建多个标记的 Feature 并添加到 VectorLayer
-      addressList.forEach(coord => {
-        const Fcoord = fromLonLat(coord) 
-        const markerFeature = new Feature({
-          geometry: new Point(Fcoord),
+      user_edit_vectorSource.addFeature(markerFeature);
+  });
+  //
+if (fetchNaviInfoTrigger) {
+  axios.post('api/ACCESSTOKEN/', { name: NAME })
+    .then(response => {
+      let MAPBOX_ACCESS_TOKEN = JSON.parse(JSON.stringify(response.data));
+      const coordinates = addressList.map(coord => `${coord[0]},${coord[1]}`).join(';');    
+      fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates}?steps=true&geometries=geojson&access_token=${MAPBOX_ACCESS_TOKEN}`)
+      .then(response => response.json())
+      .then(data => {
+        const route = data.routes[0].geometry.coordinates;
+        const lineCoords_user = route.map(coord => fromLonLat(coord));
+        const lineFeature_user = new Feature({
+          geometry: new LineString(lineCoords_user),
         });
-        markerLayer.getSource().addFeature(markerFeature);
+        user_edit_vectorSource.addFeature(lineFeature_user);
       });
-    
-          // 将 VectorLayer 添加到地图
-      map.addLayer(markerLayer);
-    // 增加一个矢量层， 用于绘制路径
-    const drawSource_user = new VectorSource();
-    const drawLayer_user = new VectorLayer({
-      source: drawSource_user,
-      title: "user_edit_route",
-      style: new Style({
-        stroke: new Stroke({
-          color: 'blue', // 设置线的颜色
-          width: 4,      // 设置线的宽度
-        }),
-      }),
+    })
+    .catch(error => {
+      console.error('获取TOKEN失败', error);
     });
-    map.addLayer(drawLayer_user);
-      if (fetchNaviInfoTrigger) {
-        
-        axios.post('api/ACCESSTOKEN/', { name: NAME })
-          .then(response => {
-            let MAPBOX_ACCESS_TOKEN = JSON.parse(JSON.stringify(response.data));
-            const coordinates = addressList.map(coord => `${coord[0]},${coord[1]}`).join(';');    
-            fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinates}?steps=true&geometries=geojson&access_token=${MAPBOX_ACCESS_TOKEN}`)
-            .then(response => response.json())
-            .then(data => {
-              const route = data.routes[0].geometry.coordinates;
-              // console.log("route_List",route);  实验用的三个坐标返回了33个坐标点，坐标点之间相互连接形成线
-              const lineCoords_user = route.map(coord => fromLonLat(coord));
-              const lineFeature_user = new Feature({
-                geometry: new LineString(lineCoords_user),
-              });
-              drawSource_user.addFeature(lineFeature_user);
-            });
-            // 添加修改线的交互功能
-          const modify_user = new Modify({
-            source: drawSource_user,
-          });
-          map.addInteraction(modify_user);
-          map.addInteraction(new Snap({ source: drawSource_user }));
-          })
-        .catch(error => {
-            console.error('获取TOKEN失败', error);
-          });
+}
+// 创建一个 VectorLayer，将 VectorSource 添加到其中
+const edit_Layer = new VectorLayer({
+  title: "Edit_route",
+  source: user_edit_vectorSource,
+  style: [
+    // 点的样式
+    new Style({
+      image: new Circle({
+        radius: 6,
+        fill: new Fill({ color: 'red' }),
+        stroke: new Stroke({ color: 'white', width: 2 }),
+      }),
+    }),
+    // 线的样式
+    new Style({
+      stroke: new Stroke({
+        color: 'blue', 
+        width: 4,      
+      }),
+    }),
+  ],
+});
+
+// 将 VectorLayer 添加到地图
+map.addLayer(edit_Layer);
     
-    }
+    
     
    
     // 销毁地图实例
